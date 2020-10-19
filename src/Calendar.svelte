d6e04385caaf0e19275b625931d3b31cb4f51a6c
Alexandre Pichon <alexandre.pichon@iadvize.com>
2020-10-19 14:16:30 +0200

    add first session to calendar

--- src/Calendar.svelte
+++ src/Calendar.svelte
@@ -2,14 +2,17 @@
 	import FullCalendar from 'svelte-fullcalendar';
 	import timeGridPlugin from '@fullcalendar/timegrid';
 
-	export let sessions;
+	export let sessions = [];
 
 	let options = {
 		initialView: 'timeGridDay',
 		plugins: [timeGridPlugin],
+		slotMinTime: '08:00:00',
+		height: 650,
+		events: sessions,
 	};
+	console.log(options);
 
-	console.log(sessions.length);
 </script>
 
 <h2>Sessions calendar</h2>
--- src/SvelteCalendarBench.svelte
+++ src/SvelteCalendarBench.svelte
@@ -2,20 +2,35 @@
 	import Config from './Config.svelte';
 	import Calendar from './Calendar.svelte';
 
-	import { sessions } from './store.js';
+	let sessions = [{
+		id: '1',
+		title: 'Session 1',
+		start: '2020-10-19T12:00:00',
+		end: '2020-10-19T14:00:00',
+		allDay: false,
+	}];
 
 	function configChanged(event){
-		$sessions = generateSessions(event.detail);
+		sessions = generateSessions(event.detail);
+		console.log(sessions);
 	}
 
 	function generateSessions(config){
-		return [];	
+    const start = new Date();
+		return [
+			{
+				id: '1',
+				title: 'Session 1',
+				start: start,
+				end: new Date(start.getTime() + config.sessionsLength * 60000),
+				allDay: false,
+			}
+		];	
 	}
-
 </script>
 
 <main>
 	<h1>Svelte calendar performance POC</h1>
 	<Config on:submit={configChanged}/>
-	<Calendar sessions />
+	<Calendar {sessions} />
 </main>
